b333122c3ddde86e330c1d4fba5e7082e0a036f9
Tan Jun Kang <2603068@sit.singaporetech.edu.sg>
2026-09-04 14:47:08 +0800

    Fun conditions

--- hello.py
+++ hello.py
@@ -31,3 +31,16 @@ print("===========================")
 print("Username:", username)
 print("Age:", age)  
 print("Category:", category)
+
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("===========================")
+print("Username:", username)
+print("Age:", age)  
+print("Category:", category)
+
+if age > 40 and category == "fun":
+    print("You are old what is fun for you??")
